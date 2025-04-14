35da2f24a103fadebcf5b2b549e52778bf20d1a0
jangteam825 <zokzokz@naver.com>
2025-04-14 23:32:11 +0900

    Delete src/components/TrainTracker.tsx

--- src/components/TrainTracker.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function TrainTracker() {
-  return (
-    <div className="mt-10 text-gray-600 text-sm">
-      <p>🔧 여기에 곧 열차 애니메이션과 시간 표시가 추가됩니다!</p>
-    </div>
-  );
-}
